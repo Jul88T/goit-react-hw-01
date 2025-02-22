8ed173e721f427898a935e79822a09c91bc7888d
Julia Tarasenko <tarasenkojul88@gmail.com>
2025-02-21 21:32:05 -0800

    did second task

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import Profile from "./components/Profile";
 import userData from "./userData.json";
+import FriendList from "./components/FriendList.jsx";
+import friends from "./friends.json";
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
         image={userData.avatar}
         stats={userData.stats}
       />
+
+      <FriendList friends={friends} />
     </>
   );
 }
